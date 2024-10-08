7141455222f5fa9b3a5650999c0aa507190a78b7
George-9 <georgemuigainjau@gmail.com>
2024-10-08 16:13:22 +0300

    increased payload size limit

--- server_app/app.js
+++ server_app/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from "cors";
-import bodyParser from 'body-parser';
 
 const app = express();
 const limit = "3000mb";
@@ -8,7 +7,6 @@ const limit = "3000mb";
 app.use(cors());
 
 app.use(express.json({ 'limit': limit }));
-app.use(bodyParser({ 'limit': limit }))
 app.use(express.urlencoded({ 'extended': true, 'limit': limit }));
 app.use(express.static('./public', { 'extensions': ['html', 'htm'], 'index': false }));
 
